61b9077fb91e375aacefba848bc0539530da4fde
Francisco G <ideabwebsites@gmail.com>
2021-08-03 19:45:26 -0400

    Top Sellers Page

--- src/Components/HomePage/ContentPage/Info.js
+++ src/Components/HomePage/ContentPage/Info.js
@@ -1,58 +1,28 @@
 import React from 'react';
-import { Col, Row, Card, Button } from 'react-bootstrap';
+import { Col, Row, Button } from 'react-bootstrap';
 import {INFO} from '../../../HomeLinks.js'
 
 
 function Info() {
   return (
     <>
+      <h3>Top Sellers</h3>
       <Row>
-        <Col md={4}>
-          <Button href={INFO[0].url}>
+        <Col md={4} >
+          <Button href={INFO[0].url} id='ProductCard'>
             <img src={INFO[0].pic} alt={INFO[0].name} width='100%'/>
-            <h3 key={INFO[0].id}>{INFO[0].name}</h3>
+            <h5 className='d-flex text-center align-content-center justify-content-center' key={INFO[0].id}>{INFO[0].name}</h5>
           </Button>
         </Col>
-        <Col md={4}>
-          <Button href={INFO[1].url}>
-            <img src={INFO[1].pic} alt={INFO[0].name} width='100%'/>
-            <h3 key={INFO[1].id}>{INFO[1].name}</h3>
-          </Button>
-        </Col>
-        <Col md={4}>
-          <Button href={INFO[0].url}>
-            <Card class=''>
-              <img src={INFO[0].pic} alt={INFO[0].name} width='100%'/>
-              <Card.Title key={INFO[0].id}>{INFO[0].name}</Card.Title>
-            </Card>
+        <Col md={4} >
+          <Button href={INFO[1].url} id='ProductCard'>
+            <img src={INFO[1].pic} alt={INFO[1].name} width='100%'/>
+            <h5 className='d-flex text-center align-content-center justify-content-center' key={INFO[1].id}>{INFO[1].name}</h5>
           </Button>
         </Col>
       </Row>
 
-      <Row>
-        <Col md={4}>
-          <Button href={INFO[0].url}>
-            <img src={INFO[0].pic} alt={INFO[0].name} width='100%'/>
-            <h3 key={INFO[0].id}>{INFO[0].name}</h3>
-          </Button>
-        </Col>
-        <Col md={4}>
-          <Button href={INFO[0].url}>
-            <Card class=''>
-              <img src={INFO[0].pic} alt={INFO[0].name} width='100%'/>
-              <Card.Title key={INFO[0].id}>{INFO[0].name}</Card.Title>
-            </Card>
-          </Button>
-        </Col>
-        <Col md={4}>
-          <Button href={INFO[0].url}>
-            <Card class=''>
-              <img src={INFO[0].pic} alt={INFO[0].name} width='100%'/>
-              <Card.Title key={INFO[0].id}>{INFO[0].name}</Card.Title>
-            </Card>
-          </Button>
-        </Col>
-      </Row>
+      
     </>
   )
 }
